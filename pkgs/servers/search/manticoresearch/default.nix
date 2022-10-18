29f27f742b09cf4e54b75184ccba86fe83e2392b
Jonathan del Strother <jdelStrother@gmail.com>
2022-10-18 13:32:01 +0100

    Alternate manticore build with more mkDerivations

--- pkgs/servers/search/manticoresearch/default.nix
+++ pkgs/servers/search/manticoresearch/default.nix
@@ -1,70 +1,87 @@
-{ stdenv, fetchFromGitHub, fetchurl, lib
-, mariadb-connector-c, boost, cmake, bison, flex
+{ lib, stdenv, fetchFromGitHub, fetchurl
+, bison, cmake, flex
+, boost, mariadb-connector-c, openssl, re2, mariadb-galera, icu, pkg-config
 }:
 
 let
-  columnar = fetchFromGitHub {
-    owner = "manticoresoftware";
-    repo = "columnar";
-    rev = "b211815f";
-    sha256 = "sha256-7oWqa1P1sGUkaTuMLWz36KhR42SjEWQo/4g+JKAsJOU=";
+  boost_static = boost.override { enableStatic = true; };
+  columnar = stdenv.mkDerivation {
+    name = "columnar";
+    src = fetchFromGitHub {
+      owner = "manticoresoftware";
+      repo = "columnar";
+      rev = "c16-s5"; # see NEED_COLUMNAR_API/NEED_SECONDARY_API in GetColumnar.cmake
+      sha256 = "sha256-iHB82FeA0rq9eRuDzY+AT/MiaRIGETsnkNPCqKRXgq8=";
+    };
+    nativeBuildInputs = [ cmake ];
+    cmakeFlags = [ "-DAPI_ONLY=ON" ];
   };
-  galera_branch = "5e7af6e5";
-  galera = fetchurl {
-    url = "https://github.com/klirichek/galera/archive/${galera_branch}.zip";
-    sha256 = "0520a49m89g2ywphvy2bdd3bwfksa9jyz4gx25aacdnwsp1mxqg9";
+  galera = stdenv.mkDerivation {
+    name = "galera";
+    src = fetchFromGitHub {
+      owner = "klirichek";
+      repo = "galera";
+      rev = "5e7af6e51ddb13a217e3cb02b79158f6202b008c";
+      sha256 = "sha256-AaKM3G0uJ9oDHWMUyom/JrMvi1omaB3aUrGHRhRe9yA=";
+      fetchSubmodules = true;
+    };
+    nativeBuildInputs = [ cmake ];
+    buildInputs = [ boost_static.dev openssl ];
   };
-  re2_branch = "2015-06-01";
-  re2 = fetchurl {
-    url = "https://github.com/manticoresoftware/re2/archive/${re2_branch}.zip";
-    sha256 = "0wr9mpv409568zl97vgxvwimbinqw7j9x6i3jyn7zw6a0lvmfzcb";
-  };
-  stemmer = fetchurl {
-    url = "https://snowballstem.org/dist/libstemmer_c.tgz";
-    sha256 = "1z2xvrjsaaypc04lwz7dg8mjm5cq1gzmn0l544pn6y2ll3r7ckh5";
-  };
-  icu = fetchurl {
-    url = "https://github.com/unicode-org/icu/releases/download/release-65-1/icu4c-65_1-src.tgz";
-    sha256 = "0j6r6qqnhfr5iqkx53k63ifkm93kv1kkb7h2mlgd1mnnndk79qsk";
+  stemmer = stdenv.mkDerivation {
+    name = "stemmer";
+    src = fetchurl {
+      url = "http://web.archive.org/web/20220616132858/https://snowballstem.org/dist/libstemmer_c.tgz";
+      sha256 = "1z2xvrjsaaypc04lwz7dg8mjm5cq1gzmn0l544pn6y2ll3r7ckh5";
+    };
+    installPhase = ''
+      mkdir -p $out/bin $out/include $out/lib
+      cp stemwords $out/bin/
+      cp include/* $out/include/
+      cp libstemmer.o $out/lib/
+    '';
   };
 in
 
 stdenv.mkDerivation rec {
   pname = "manticoresearch";
-  version = "5.0.2";
+  version = "5.0.3";
 
   src = fetchFromGitHub {
     owner = "manticoresoftware";
     repo = "manticoresearch";
     rev = version;
-    sha256 = "sha256-FRCy/XOlIiScIf5XO/DG5lkBdToT2xrZLsC5TsK5SQw=";
+    sha256 = "sha256-samZYwDYgI9jQ7jcoMlpxulSFwmqyt5bkxG+WZ9eXuk=";
   };
 
   nativeBuildInputs = [
+    pkg-config
     bison
     cmake
     flex
   ];
 
   buildInputs = [
-    (boost.override { enableStatic = true; })
+    boost_static
     mariadb-connector-c
+    galera
+    icu.dev
+    re2
+    columnar
   ];
-
-  COLUMNAR_LOCATOR = "SOURCE_DIR ${columnar}";
-
-  postUnpack = ''
-    LIBS_BUNDLE="$NIX_BUILD_TOP/bundle"
-    mkdir "$LIBS_BUNDLE"
-    cp "${galera}" "$LIBS_BUNDLE/galera-${galera_branch}.zip"
-    cp "${re2}" "$LIBS_BUNDLE/re2-${re2_branch}.zip"
-    cp "${stemmer}" "$LIBS_BUNDLE/libstemmer_c.tgz"
-    cp "${icu}" "$LIBS_BUNDLE/icu4c-65_1-src.tgz"
-  '';
 
   cmakeFlags = [
     "-DMYSQL_CONFIG_EXECUTABLE=${mariadb-connector-c}/bin/mysql_config"
     "-DMYSQL_INCLUDE_DIR=${mariadb-connector-c.dev}/include/mariadb"
+
+    "-DWITH_RE2_FORCE_STATIC=0"
+    "-DRE2_LIBRARY=${re2}/lib/libre2.a"
+
+    "-DWITH_STEMMER_FORCE_STATIC=0"
+    "-DSTEMMER_LIBRARY=${stemmer}/lib/libstemmer.o"
+    "-DSTEMMER_INCLUDE_DIR=${stemmer}/include"
+
+    "-DWITH_ICU_FORCE_STATIC=0"
   ];
 
   meta = {
