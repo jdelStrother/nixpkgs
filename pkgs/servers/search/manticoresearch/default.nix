48a015e6d78940de042b3e6499f39e0332ac0e2f
Jonathan del Strother <jdelStrother@gmail.com>
2022-10-21 17:24:47 +0100

    avoid manticore's with_get cmake functionality entirely

--- pkgs/servers/search/manticoresearch/default.nix
+++ pkgs/servers/search/manticoresearch/default.nix
@@ -1,10 +1,8 @@
 { lib, stdenv, fetchFromGitHub, fetchurl
-, bison, cmake, flex
-, boost, icu, mariadb-connector-c, re2
-    ,pkg-config
+, bison, cmake, flex, pkg-config
+, boost, icu, libstemmer, mariadb-connector-c, re2
 }:
 let
-  boost_static = boost.override { enableStatic = true; };
   columnar = stdenv.mkDerivation rec {
     pname = "columnar";
     version = "c16-s5"; # see NEED_COLUMNAR_API/NEED_SECONDARY_API in Manticore's GetColumnar.cmake
@@ -23,26 +21,6 @@ let
       platforms = lib.platforms.all;
     };
   };
-  stemmer = stdenv.mkDerivation {
-    pname = "libstemmer";
-    version = "2.0.0";
-    src = fetchurl {
-      url = "http://web.archive.org/web/20220616132858/https://snowballstem.org/dist/libstemmer_c.tgz";
-      sha256 = "1z2xvrjsaaypc04lwz7dg8mjm5cq1gzmn0l544pn6y2ll3r7ckh5";
-    };
-    installPhase = ''
-      mkdir -p $out/bin $out/include $out/lib
-      cp stemwords $out/bin/
-      cp include/* $out/include/
-      cp libstemmer.o $out/lib/
-    '';
-    meta = {
-      description = "Library for working with snowball stemming algorithms";
-      homepage = "https://snowballstem.org";
-      license = lib.licenses.bsd3;
-      platforms = lib.platforms.all;
-    };
-  };
 in
 stdenv.mkDerivation rec {
   pname = "manticoresearch";
@@ -56,52 +34,37 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [
-    pkg-config
     bison
     cmake
     flex
-  ];
-
-  buildInputs = [
-    boost_static
-    columnar
-    icu.dev
-    mariadb-connector-c
-    re2
     pkg-config
   ];
 
+  buildInputs = [
+    boost
+    columnar
+    icu.dev
+    libstemmer
+    mariadb-connector-c
+    re2
+  ];
+
   postPatch = ''
-    sed -i '1s/^/set(CMAKE_FIND_DEBUG_MODE TRUE)/' cmake/GetRE2.cmake
-    substituteInPlace cmake/GetRE2.cmake \
-        --replace 'find_package ( re2 MODULE QUIET )' 'find_package ( re2 MODULE REQUIRED )'
-    substituteInPlace cmake/GetRE2.cmake \
-        --replace 'find_package ( re2 QUIET CONFIG )' 'find_package ( re2 CONFIG REQUIRED )'
+    sed -i 's/set ( Boost_USE_STATIC_LIBS ON )/set ( Boost_USE_STATIC_LIBS OFF )/' src/CMakeLists.txt
+
+    # supply our own packages rather than letting manticore download dependencies during build
+    sed -i 's/^with_get/with_menu/' CMakeLists.txt
   '';
 
   cmakeFlags = [
-    "-DMYSQL_CONFIG_EXECUTABLE=${mariadb-connector-c}/bin/mysql_config"
-    "-DMYSQL_INCLUDE_DIR=${mariadb-connector-c.dev}/include/mariadb"
-    "-DRE2_LIBRARY=${re2}/lib/libre2.a"
-    "-Dre2_DIR=${re2}/include"
-    "-DRE2_DIR=${re2}/include"
-    "-Dre2_LIBRARY=${re2}/lib/libre2.a"
-    "-Dre2_INCLUDE_DIR=${re2}/include"
-    "-DRE2_LIBRARY=${re2}/lib/libre2.a"
-    "-DRE2_INCLUDE_DIR=${re2}/include"
-    "-DSTEMMER_LIBRARY=${stemmer}/lib/libstemmer.o"
-    "-DSTEMMER_INCLUDE_DIR=${stemmer}/include"
     "-DWITH_GALERA=0"
-    "-DWITH_ICU_FORCE_STATIC=0"
-    # "-DWITH_RE2_FORCE_STATIC=0"
-    "-DWITH_STEMMER_FORCE_STATIC=0"
   ];
 
-  meta = {
+  meta = with lib; {
     description = "Easy to use open source fast database for search";
     homepage = "https://manticoresearch.com";
-    license = lib.licenses.gpl2;
-    maintainers = with lib.maintainers; [ jdelStrother ];
-    platforms = lib.platforms.all;
+    license = licenses.gpl2;
+    maintainers = with maintainers; [ jdelStrother ];
+    platforms = platforms.all;
   };
 }
